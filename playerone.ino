becdee01e54b5cf8740735dba28d392e4c46c70e
Max Walters <max.walters10@gmail.com>
2018-04-06 12:03:25 -0400

    Adding setup

--- playerone.ino
+++ playerone.ino
@@ -9,3 +9,21 @@ const int kLeft = 0;
 const int kRight = 1;
 const int kUp = 2;
 const int kAttack = 14;
+
+const double kButtonInterval = 10;
+
+Bounce btn = Bounce();
+
+void setup() {
+  // Set the pin for the Bounce, and pull the signal up.
+  // If the signal is PULLUP by default, 'falling' is pressed.
+  btn.attach(kUp, INPUT_PULLUP);
+
+  // Set the interval for the pin
+  btn.interval(kButtonInterval);
+
+  delay(500);
+
+  pinMode(kLeft, INPUT_PULLUP);
+  pinMode(kRight, INPUT_PULLUP);
+}
